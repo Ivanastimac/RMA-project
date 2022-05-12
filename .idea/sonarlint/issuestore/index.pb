5c4eb0fa498c92136e8c1ea430c6df6d5b3bfc98
Ivanastimac <ivana.stimac008@gmail.com>
2022-05-12 18:57:14 +0200

    Added updating additional user info

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,5 +7,11 @@ H
 build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
 @
 app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
-P
- app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+e
+5app/src/main/java/com/example/project/model/User.java,5\8\58f5c2af121041d321a4d723355dbec8e4524cc5
+m
+=app/src/main/java/com/example/project/user_profile/Login.java,4\6\46a64ada2fb8930c07954c1103b5f89ed653d3fa
+v
+Fapp/src/main/java/com/example/project/user_profile/ChangePassword.java,f\c\fcccbc6f3ff19f152bbc115e478d494cd8cbb3ba
+u
+Eapp/src/main/java/com/example/project/user_profile/PasswordReset.java,5\a\5a180d4e07267dc72410edb10a1ecec915c939ff
--- app/src/main/java/com/example/project/user_profile/Settings.java
+++ app/src/main/java/com/example/project/user_profile/Settings.java
@@ -5,12 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.project.MainActivity;
 import com.example.project.R;
 import com.example.project.model.User;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -28,10 +30,16 @@ public class Settings extends AppCompatActivity {
     TextView firstName;
     TextView lastName;
     TextView email;
+    EditText firstNameEdit;
+    EditText lastNameEdit;
+    EditText emailEdit;
     Button changePasswordBtn;
-    Button changeEmailBtn;
     Button deleteAccountBtn;
     Button signOutBtn;
+    Button saveEditedBtn;
+    ImageView editBtn;
+    User user;
+
     FirebaseAuth auth;
     FirebaseUser loggedInUser;
     FirebaseAuth.AuthStateListener authListener;
@@ -55,31 +63,36 @@ public class Settings extends AppCompatActivity {
             }
         };
 
-        auth = FirebaseAuth.getInstance();
-        loggedInUser = auth.getCurrentUser();
-
-        // if user is not logged in, redirect to login page
-        if (loggedInUser == null) {
-            Intent in = new Intent(this, Login.class);
-            startActivity(in);
-        }
-
         firstName = findViewById(R.id.textViewFirstName);
         lastName = findViewById(R.id.textViewLastName);
         email = findViewById(R.id.textViewEmail);
         changePasswordBtn = findViewById(R.id.buttonChangePassword);
-        changeEmailBtn = findViewById(R.id.buttonChangeEmail);
         deleteAccountBtn = findViewById(R.id.buttonDeleteAccount);
         signOutBtn = findViewById(R.id.buttonSignOut);
+        editBtn = findViewById(R.id.imageButtonEditInfo);
+
+        firstNameEdit = findViewById(R.id.editTextFirstName);
+        lastNameEdit = findViewById(R.id.editTextLastName);
+        emailEdit = findViewById(R.id.editTextEmail);
+        saveEditedBtn = findViewById(R.id.buttonSaveEdited);
+
+        // editviews for updating info, visible after clicking edit button
+        firstNameEdit.setVisibility(View.GONE);
+        lastNameEdit.setVisibility(View.GONE);
+        emailEdit.setVisibility(View.GONE);
+        saveEditedBtn.setVisibility(View.GONE);
 
         DatabaseReference database = FirebaseDatabase.getInstance().getReference("users");
+
+        auth = FirebaseAuth.getInstance();
+        loggedInUser = auth.getCurrentUser();
         email.setText(loggedInUser.getEmail());
 
         // get logged in user info and display it on screen
         database.child(loggedInUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                User user = dataSnapshot.getValue(User.class);
+                user = dataSnapshot.getValue(User.class);
                 firstName.setText(user.getFirstName());
                 lastName.setText(user.getLastName());
             }
@@ -90,23 +103,64 @@ public class Settings extends AppCompatActivity {
             }
         });
 
+        editBtn.setOnClickListener(view -> {
+                firstName.setVisibility(View.GONE);
+                lastName.setVisibility(View.GONE);
+                email.setVisibility((View.GONE));
+                firstNameEdit.setVisibility(View.VISIBLE);
+                lastNameEdit.setVisibility(View.VISIBLE);
+                emailEdit.setVisibility(View.VISIBLE);
+                saveEditedBtn.setVisibility(View.VISIBLE);
+                firstNameEdit.setText(user.getFirstName());
+                lastNameEdit.setText(user.getLastName());
+                emailEdit.setText(loggedInUser.getEmail());
+        });
+
+        saveEditedBtn.setOnClickListener(view -> {
+
+            if (checkEnteredData()) {
+                // first update email through firebase auth method
+                loggedInUser.updateEmail(emailEdit.getText().toString().trim())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Settings.this, "Email address is updated.", Toast.LENGTH_LONG).show();
+                                } else {
+                                    Toast.makeText(Settings.this, "Failed to update email!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                // update additional user info
+                database.child(loggedInUser.getUid()).child("firstName").setValue(firstNameEdit.getText().toString());
+                database.child(loggedInUser.getUid()).child("lastName").setValue(lastNameEdit.getText().toString());
+
+                // return to user info display, without edit functionality
+                firstNameEdit.setVisibility(View.GONE);
+                lastNameEdit.setVisibility(View.GONE);
+                emailEdit.setVisibility(View.GONE);
+                saveEditedBtn.setVisibility(View.GONE);
+                firstName.setVisibility(View.VISIBLE);
+                lastName.setVisibility(View.VISIBLE);
+                email.setVisibility(View.VISIBLE);
+            }
+
+        });
+
         changePasswordBtn.setOnClickListener(view -> {
             Intent in = new Intent(view.getContext(), ChangePassword.class);
             view.getContext().startActivity(in);
         });
 
-        changeEmailBtn.setOnClickListener(view -> {
-            Intent in = new Intent(view.getContext(), ChangeEmail.class);
-            view.getContext().startActivity(in);
-        });
-
         deleteAccountBtn.setOnClickListener(view -> {
+            String uid = loggedInUser.getUid();
             loggedInUser.delete()
                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
                         public void onComplete(@NonNull Task<Void> task) {
                             if (task.isSuccessful()) {
                                 Toast.makeText(Settings.this, "Your profile is deleted:( Create a account now!", Toast.LENGTH_SHORT).show();
+                                database.child(uid).removeValue();
                                 Intent in = new Intent(view.getContext(), Login.class);
                                 view.getContext().startActivity(in);
                             } else {
@@ -121,6 +175,34 @@ public class Settings extends AppCompatActivity {
             Intent in = new Intent(view.getContext(), Login.class);
             view.getContext().startActivity(in);
         });
+
+    }
+
+    boolean checkEnteredData() {
+
+        boolean valid = true;
+
+        if (firstNameEdit == null || firstNameEdit.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter your first name.", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else if (firstNameEdit.getText().length() < 3 || firstNameEdit.getText().length() > 20  /*|| !NAME_PATTERN.matcher(firstName.getText().toString()).matches()*/) {
+            Toast.makeText(this, "Please enter valid first name.", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else if (lastNameEdit == null || lastNameEdit.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter your last name.", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else if (lastNameEdit.getText().length() < 3 || firstNameEdit.getText().length() > 20 /*|| !NAME_PATTERN.matcher(lastName.getText().toString()).matches()*/) {
+            Toast.makeText(this, "Please enter valid last name.", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else if (emailEdit == null || emailEdit.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Please enter new e-mail address.", Toast.LENGTH_SHORT).show();
+            valid = false;
+        } else if (!Patterns.EMAIL_ADDRESS.matcher(emailEdit.getText().toString()).matches()) {
+            Toast.makeText(this, "E-mail address is not valid!", Toast.LENGTH_SHORT).show();
+            valid = false;
+        }
+
+        return valid;
 
     }
 
